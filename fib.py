eadee9c037aab3cbd1c06312ee00fbf86724a214
student <student@USProgrammingCart4.local>
2017-10-16 13:41:33 -0400

    fib

--- fib.py
+++ fib.py
@@ -1,8 +1,12 @@
+import sys
+
 def fib(n):
-    a=0
-    b=1
+    if n==1: c=0
+    else: a,b,c=0,1,1
     for i in range(n-2):
         c=a+b
-        a=b
-        b=c
+        a,b=b,c
     print c
+
+if __name__ == "__main__":
+    fib(int(sys.argv[1]))
