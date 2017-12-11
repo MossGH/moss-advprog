f109e72cf293535a28f0cfac3e703038a11b4fdc
jmoss2020 <31778027+jmoss2020@users.noreply.github.com>
2017-12-11 13:41:35 -0500

    should be done

--- Garage.py
+++ Garage.py
@@ -12,17 +12,45 @@ class Car():
     def park(self, name):
         if len(name.spots) != name.size:
             name.spots.append(self)
+        else: print "That garage is full."
 
     def __str__(self):
         return self.owner + ":" + str(self.registration)
 
-cars=[]
-garages=[]
+cars={}
+garages={}
 q=0
 while q!="Q":
-    q=raw_input("(A)dd a person, (L)ook at a person, (V)iew exeryone, count (S)tudents or (T)eachers, or (Q)uit: ").upper()
-    if q=="A":
-        owner=raw_input("Owner: ")
-        registration=raw_input("Refistration: ")
-        cars.append(Car(owner, registration))
-###The string of choises is wrong###
+    q=raw_input("Add a (C)ar, add a (G)arage, (P)ark a car, (Q)uit: ").upper()
+    if q=="C":
+        d=True
+        while d:
+            registration=raw_input("Registration: ")
+            if registration not in cars:
+                cars[registration]=Car(raw_input("Owner: "), registration)
+                d=False
+            else: print "Registration in use"
+    elif q=="G":
+        d=True
+        while d:
+            name=raw_input("Garage name: ")
+            if name not in garages:
+                garages[name]=Garage(name, input("Number of parking spots: "))
+                d=False
+            else: print "Garage name in use"
+    elif q=="P":
+        d=True
+        while d:
+            try:
+                registration=raw_input("Registration: ")
+                if registration in cars:
+                    d=False
+            except: print "Invalid registration"
+        d=True
+        while d:
+            try:
+                name=raw_input("Garage name: ")
+                if name in garages:
+                    d=False
+            except: print "Invalid name"
+        cars[registration].park(garages[name])
